43b98e0d9ededb59bd32610969a83d75d7e60c7e
aoliva16 <42818554+aoliva16@users.noreply.github.com>
2019-11-20 13:24:25 -0500

    Update index.php

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 	<title> 
-		DoD Vehicle Registration Tool
+		STRS Vehicle Registration Tool
 	</title>
 
 
@@ -46,7 +46,7 @@
 
 <div class="intro">
 
-	<h1> DoD Vehicle Tool</h1>
+	<h2> STRS Vehicle Tool</h2>
 
 	<!-- Display redundant error message on top of webpage if there is an error -->
 	<h3> <span class="error"> <?php echo $prevSelections['errorMessage'] ?> </span> </h3>
