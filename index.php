d2813d355d74843b96071ef5807199458047ea14
aoliva16 <42818554+aoliva16@users.noreply.github.com>
2020-07-16 16:53:06 -0400

    Updated index.php - changed names
    Changed Header names to reflect new customer.

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 
 	<title> 
-		Department of State
+		LNLL
 	</title>
 
 
@@ -46,7 +46,7 @@
 
 <div class="intro">
 
-	<h2> Department of State - Vehicle Registration Tool</h2>
+	<h2>LNLL - Vehicle Registration Tool</h2>
 
 	<!-- Display redundant error message on top of webpage if there is an error -->
 	<h3> <span class="error"> <?php echo $prevSelections['errorMessage'] ?> </span> </h3>
